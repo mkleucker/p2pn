67014ab5b53374bf77fbf7f5d1f6d0dc85ec84a8
Max Kleucker <max.kleucker@gmail.com>
2014-01-30 18:16:47 +0100

    Message parsing.

--- max/cli/src/p2pn/Main.java
+++ max/cli/src/p2pn/Main.java
@@ -1,5 +1,10 @@
 package p2pn;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Main class to later run the peer that also is responsible for
  * taking input from the commandline and issuing the correct commands
@@ -8,11 +13,11 @@ package p2pn;
  */
 public class Main {
 
-    private Object peer;
+    //private Object peer;
 
     /**
      * Default Java init method
-     * @param args
+     * @param args String array providing peer ID and port.
      */
     public static void main(String[] args){
         System.out.println("Initiating peer...");
@@ -21,7 +26,11 @@ public class Main {
             System.err.println("Too few arguments. Make sure to provide the Peer ID and the desired port to run on.");
         }
 
-        System.out.print(">");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        parseInput(br);
+
+
+
     }
 
     /**
@@ -35,7 +44,20 @@ public class Main {
         //this.peer.shutdown();
     }
 
-    private void parseInput(){
+    private static void parseInput(BufferedReader br){
+        String input;
+        try {
+            System.out.print(">");
+            input = br.readLine();
+            System.out.println(input);
+            if(input.equals("exit")){
+                System.exit(0);
+            }
+            parseInput(br);
+        } catch (IOException ioe) {
+            System.out.println("IO error!");
+            System.exit(1);
+        }
 
     }
 
