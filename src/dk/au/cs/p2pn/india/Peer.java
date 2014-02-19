24450f6a62069a2df7b8b6429344aa9c56d0808f
Max Kleucker <max.kleucker@gmail.com>
2014-02-19 14:59:01 +0100

    Correct implementation of equals() and hashcode() for the Peer object.

--- src/dk/au/cs/p2pn/india/Peer.java
+++ src/dk/au/cs/p2pn/india/Peer.java
@@ -5,22 +5,22 @@ public class Peer {
 	String ip; // the IP 
 	int port; // the port number  
 	int capacity; // the maximum number of neighbors
-	
+
 	/**
 	 * Constructor of the class Peer
-	 * 
-	 * @param id ID of the peer
-	 * @param ip IP of the peer
-	 * @param port Port of the peer
+	 *
+	 * @param id       ID of the peer
+	 * @param ip       IP of the peer
+	 * @param port     Port of the peer
 	 * @param capacity Capacity of the peer
 	 */
-	public Peer(int id, String ip, int port, int capacity){
+	public Peer(int id, String ip, int port, int capacity) {
 		this.id = id; // the id number for each Peer
 		this.ip = ip; // the IP 
 		this.port = port; // the port number  
 		this.capacity = capacity; // the maximum number of neighbors
 	}
-		
+
 	public int getId() {
 		return id;
 	}
@@ -48,9 +48,22 @@ public class Peer {
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
-	
-	public boolean equals(Peer p) {
-		return p.getId() == this.getId();
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Peer peer = (Peer) o;
+
+		if (id != peer.id) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
 	}
 
 	public boolean smallerThan(Peer p) {
