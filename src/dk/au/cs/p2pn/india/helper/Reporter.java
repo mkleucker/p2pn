e4bad291fb929ee1a03d0ab4dbdc9c63de42acca
Max Kleucker <max.kleucker@gmail.com>
2014-02-20 17:06:25 +0100

    Switched to an Enum and HashMap for tracking events.

--- src/dk/au/cs/p2pn/india/helper/Reporter.java
+++ src/dk/au/cs/p2pn/india/helper/Reporter.java
@@ -1,32 +1,21 @@
 package dk.au.cs.p2pn.india.helper;
 
 
+import java.util.HashMap;
+
 public class Reporter {
 
-	/**
-	 * Number of incoming messages this per has processed.
-	 */
-	private int messagesReceived;
-	/**
-	 * Number of search queries received from other peers.
-	 */
-	private int searchesReceived;
-	/**
-	 * Number of other peers' search queries processed.
-	 */
-	private int searchesProcessed;
-	/**
-	 * Number of search request that have been answered successful ("I have this file").
-	 */
-	private int searchesRespondedSuccessful;
+	private HashMap<ReporterMeasurements, Integer> data;
 
-	/**
-	 * Searches started by this peer.
-	 */
-	private int searchesStarted;
-	/**
-	 * Searches started by this peer that were answered successful.
-	 */
-	private int searchesSuccessfull;
+	public Reporter() {
+		this.data = new HashMap<ReporterMeasurements, Integer>();
+		for (ReporterMeasurements key : ReporterMeasurements.values()) {
+			this.data.put(key, 0);
+		}
+	}
+
+	public void addEvent(ReporterMeasurements event) {
+		this.data.put(event, this.data.get(event)+1);
+	}
 
 }
---
+++ src/dk/au/cs/p2pn/india/helper/ReporterMeasurements.java
@@ -0,0 +1,13 @@
+package dk.au.cs.p2pn.india.helper;
+
+/**
+ * Created by max on 20/02/14.
+ */
+public enum ReporterMeasurements {
+	MESSAGE_RECEIVED,
+	SEARCH_RECEIVED,
+	SEARCH_PROCESSED,
+	SEARCH_RESPONDED_SUCCESSFUL,
+	SEARCH_STARTED,
+	SEARCH_SUCCESSFUL
+}
